ff637cb5563ee0000b04e6c75d577d8ec00b04a4
nikonovxyz <72874721+nikonovxyz@users.noreply.github.com>
2023-09-27 17:57:23 +0300

    Fixed autorun

--- autorun.sh
+++ autorun.sh
@@ -1,9 +1,11 @@
 ls -1 ./motifs/pwm/ > ./scripts/tf_list.txt
-ls -1R ./motifs/pwm | grep "\.pwm$" > motifs_list.txt
-python3 ./scripts/ape_list.py
-python3 ./scripts/selex_benchmark_list.py
-sed 's/\/selex/\/adastra/g' ./scripts/selex_benchmark_list.txt ./scripts/adastra_benchmark_list.txt
-cat ./scripts/ape_list.txt | parallel -j ./procfile &> ./logs/ape.log
-cat ./scripts/selex_benchmark_list.txt | parallel -j ./procfile > ./logs/selex_messages.log 2> ./logs/selex_errors.log
-cat ./scripts/adastra_benchmark_list.txt | parallel -j ./procfile > ./logs/adastra_messages.log 2> ./logs/adastra_errors.log
+ls -1R ./motifs/pwm | grep "\.pwm$" > ./scripts/motifs_list.txt
+cd ./scripts
+python3 ./ape_list.py
+python3 ./selex_benchmark_list.py
+sed 's/\/selex/\/adastra/g' ./selex_benchmark_list.txt > ./adastra_benchmark_list.txt
+cat ./ape_list.txt | parallel -j ../procfile &> ../logs/ape.log
+cat ./selex_benchmark_list.txt | parallel -j ../procfile > ../logs/selex_messages.log 2> ../logs/selex_errors.log
+cat ./adastra_benchmark_list.txt | parallel -j ../procfile > ../logs/adastra_messages.log 2> ../logs/adastra_errors.log
+cd ..
 mv ./scripts/*tsv ./results
